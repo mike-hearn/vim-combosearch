8acc6d3d6b89a3145517cefceac28c6d99721aa4
Michael Hearn <git@mikehearn.net>
2019-07-24 22:36:45 -0400

    Minor change to args array

--- plugin/search.sh
+++ plugin/search.sh
@@ -12,6 +12,6 @@ search_matches() {
 	rg --column --hidden --max-columns=500 --line-number --ignore-case --no-heading --color=always "$*"
 }
 
-search_by_path "$*"
+search_by_path "$@"
 search_all_lines_except_matches "$@"
-search_matches "$*"
+search_matches "$@"
