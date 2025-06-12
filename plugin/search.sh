9b1abaac41e747ab8cce4ef62f5da644a80e5d1f
Michael Hearn <git@mikehearn.net>
2025-06-12 13:24:27 -0400

    Fix some search logic

--- plugin/search.sh
+++ plugin/search.sh
@@ -19,19 +19,15 @@ colorize_output() {
 }
 
 callgitgrepcommand() {
-	git --no-pager ls-files -cmo --exclude-standard ":(icase)**$SEARCH_QUERY**" | LC_ALL=C sed "s/$/:0:0/g" | colorize_file_output
-	awk '!x[$0]++' <(
-		(git --no-pager grep -I -n -i --untracked "$SEARCH_QUERY" | LC_ALL=C sed -E "s/:([0-9]+):/:\1:0:/g" | colorize_output)
-		(git --no-pager grep -I -n --untracked "[A-Za-z0-9]" ":(icase)**$SEARCH_QUERY**" | LC_ALL=C sed -E "s/:([0-9]+):/:\1:0:/g" | colorize_output)
-	)
+	git --no-pager ls-files -cmo --exclude-standard ":(icase)**$SEARCH_QUERY**" | LC_ALL=C sed "s/$/:0:0/g" | colorize_file_output | awk '!x[$0]++'
+	git --no-pager grep -I -n -i --untracked "$SEARCH_QUERY" | LC_ALL=C sed -E "s/:([0-9]+):/:\1:0:/g" | colorize_output | awk '!x[$0]++'
+	git --no-pager grep -I -n --untracked "[^[:space:]]" ":(icase)**$SEARCH_QUERY**" | LC_ALL=C sed -E "s/:([0-9]+):/:\1:0:/g" | colorize_output | awk '!x[$0]++'
 }
 
 callgrepcommand() {
-	awk '!x[$0]++' <(
-		(find . -type f -ipath "*$SEARCH_QUERY*" | LC_ALL=C sed "s/$/:0:0/g" | colorize_file_output)
-		(grep -rIHin "$SEARCH_QUERY" . | LC_ALL=C sed -E "s/:([0-9]+):/:\1:0:/g" | colorize_output)
-		(grep -rIHin --include "*$SEARCH_QUERY*" "[A-Za-z0-9]" . | LC_ALL=C sed -E "s/:([0-9]+):/:\1:0:/g" | colorize_output)
-	)
+	find . -type f -ipath "*$SEARCH_QUERY*" | LC_ALL=C sed "s/$/:0:0/g" | colorize_file_output | awk '!x[$0]++'
+	grep -rIHin "$SEARCH_QUERY" . | LC_ALL=C sed -E "s/:([0-9]+):/:\1:0:/g" | colorize_output | awk '!x[$0]++'
+	grep -rIHin --include "*$SEARCH_QUERY*" "[^[:space:]]" . | LC_ALL=C sed -E "s/:([0-9]+):/:\1:0:/g" | colorize_output | awk '!x[$0]++'
 }
 
 if (git -C . rev-parse 2>/dev/null) && [ "$2" != 'all' ]; then
